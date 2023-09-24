8e08b91a0861a142fca45fe2d17779711d8dc5ed
Harish S <harish.sq@gmail.com>
2023-09-24 19:09:47 +0530

    adding new files

--- Ascendion_udemy_Vue/Project 3 - Todo List - Initial/scripts.js
+++ Ascendion_udemy_Vue/Project 3 - Todo List - Initial/scripts.js
@@ -14,13 +14,18 @@ const todosApp = {
                 this.newTodo = {
                     done: false
                 };
-                localStorage.setItem("todos",JSON.stringify(this.todos));
+                localStorage.setItem("todos", JSON.stringify(this.todos));
             } else {
                 alert("The text field must be filled")
             }
         },
-        beforeC
+    },
+    created() {
+        this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos; 1
+    },
+    updated() {
+        localStorage.setItem("todos", JSON.stringify(this.todos));
     }
 }
-
+  
 Vue.createApp(todosApp).mount('#app');
